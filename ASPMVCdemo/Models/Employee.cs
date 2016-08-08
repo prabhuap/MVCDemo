8de5f7f87fbdcb934882dbd3c83993a5fb83fb90
prabhuslive@gmail.com <maloos@123>
2016-08-08 20:05:18 +0530

    New MVC Demo

---
+++ ASPMVCdemo/Models/Employee.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+
+
+namespace ASPMVCdemo.Models
+{
+    public class Employee
+    {
+        [Key]
+        public int E_id { get; set; }
+        public string E_name { get; set; }
+        public string E_code { get; set; }
+        public double E_salary { get; set; }
+
+    }
+
+    public class Employee_context:DbContext
+    {
+        public DbSet<Employee> Employee_set { get; set; }
+    }
+}
